ac7b25aefb0c52559d60219fff476d66aeaea6e5
Damian <damianbialkowski8@gmail.com>
2023-01-14 23:48:21 +0100

    fix required password for user update

--- Http/Requests/Frontend/CmsUserRequest.php
+++ Http/Requests/Frontend/CmsUserRequest.php
@@ -13,11 +13,18 @@ class CmsUserRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'username' => 'required|unique:cms_users,username|min:2|max:255',
-            'email' => 'required|email|max:255|unique:cms_users,email',
-            'password' => 'required|confirmed|min:6',
+        $passwordRequired = true;
+        if (!$this->request->get('password')) {
+            $passwordRequired = false;
+        }
+        $rules = [
+            'username' => 'required|unique:cms_users,username,' . $this->user()->id . '|min:2|max:255',
+//            'email' => 'required|email|max:255|unique:cms_users,email',
         ];
+        if ($passwordRequired) {
+            $rules['password'] = 'required|confirmed|min:6';
+        }
+        return $rules;
     }
 
     /**
